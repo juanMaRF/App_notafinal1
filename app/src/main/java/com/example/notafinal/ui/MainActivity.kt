cbf3c6cc092135c71e8bf6a437425acf6d67562c
Hp <juanm.rivera@udea.edu.co>
2023-03-18 14:17:31 -0500

    App de nota final

---
+++ app/src/main/java/com/example/notafinal/ui/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.notafinal.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.notafinal.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private var nota1:Double = 0.0
+    private var nota2:Double = 0.0
+    private var nota3:Double = 0.0
+    private var nota4:Double = 0.0
+    private var definitiva:Double = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val screenSplash = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        mainBinding.calcuButton.setOnClickListener {
+            nota1 = mainBinding.sesentaNoteEditText.text.toString().toDouble()
+            nota2 = mainBinding.sieteNoteEditText.text.toString().toDouble()
+            nota3 = mainBinding.ochoNoteEditText.text.toString().toDouble()
+            nota4 = mainBinding.twentyfiveNoteEditText.text.toString().toDouble()
+
+            nota1 = nota1 * 0.6
+            nota2 = nota2 * 0.07
+            nota3 = nota3 * 0.08
+            nota4 = nota4 * 0.25
+
+            definitiva = nota1 + nota2 + nota3 + nota4
+            mainBinding.definitivaTextview.text = definitiva.toString()
+        }
+
+        setContentView(mainBinding.root)
+
+        screenSplash.setKeepOnScreenCondition { false }
+    }
+}
